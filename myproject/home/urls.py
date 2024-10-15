0fdf6ca30be8f2d6836e78faa95fbb0f9277d819
LaneyS05 <laney.staggs05@gmail.com>
2024-10-15 11:10:46 -0400

    Fix routing and static files

--- myproject/home/urls.py
+++ myproject/home/urls.py
@@ -1,14 +1,10 @@
-# home/urls.py
-
 from django.urls import path
-from . import views  # Import views from the current package
+from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('links/', views.link_page, name='links'),
     path('about/', views.about_page, name='about'),
     path('name/', views.name_page, name='name'),
-    #path('send-email/', views.send_email, name='send_email'),  # Assuming you have this URL
 ]
-
 

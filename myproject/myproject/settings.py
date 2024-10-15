f5c14f0e00ccdcb3b1b96e67da9fa0efd248f7b8
LaneyS05 <laney.staggs05@gmail.com>
2024-10-15 17:19:27 -0400

    setting

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -27,11 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'your-default-secret-key')
 DEBUG = False
 #DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
-
-
-
-
+ALLOWED_HOSTS = ['my-python-app-flax.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition

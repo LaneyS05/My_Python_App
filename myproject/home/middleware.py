7c9a32700c4573214d5ea58b7ad12e0543e814d8
LaneyS05 <laney.staggs05@gmail.com>
2024-10-29 12:17:58 -0400

    middleware

--- myproject/home/middleware.py
+++ myproject/home/middleware.py
@@ -1,7 +1,11 @@
+import logging
 from django.http import HttpResponse
-from django.utils.deprecation import MiddlewareMixin
+from django.utils.deprecation import MiddlewareMixin  # Import MiddlewareMixin
+
+logger = logging.getLogger(__name__)
 
 class FaviconMiddleware(MiddlewareMixin):
     def process_request(self, request):
         if request.path == '/favicon.ico':
+            logger.info("Favicon requested, returning 204")
             return HttpResponse(status=204)  # No Content
